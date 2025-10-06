c5057b82fc77ed0ed173fbe51bf1669fb472755a
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2025-10-06 10:57:14 +0200

    Use request localization

--- DevExtremeAspNetCoreApp1/Program.cs
+++ DevExtremeAspNetCoreApp1/Program.cs
@@ -5,7 +5,18 @@ builder.Services
     .AddRazorPages()
     .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
+// Configure localization settings
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.SetDefaultCulture("it-IT");
+    options.AddSupportedCultures("it-IT", "en-US");
+    options.AddSupportedUICultures("it-IT", "en-US");
+    options.FallBackToParentUICultures = true;
+});
+
 var app = builder.Build();
+
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
