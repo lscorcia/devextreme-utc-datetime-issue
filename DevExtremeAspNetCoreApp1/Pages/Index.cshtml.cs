f5bfafcfd20d652e3f0b5dfc55a5d1c15b2f4436
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2025-10-06 10:56:18 +0200

    Implement CRUD

--- DevExtremeAspNetCoreApp1/Pages/Index.cshtml.cs
+++ DevExtremeAspNetCoreApp1/Pages/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
+using DevExtremeAspNetCoreApp1.Helpers;
 using DevExtremeAspNetCoreApp1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace DevExtremeAspNetCoreApp1.Pages {
     public class IndexModel : PageModel {
@@ -17,6 +20,34 @@ namespace DevExtremeAspNetCoreApp1.Pages {
         public IActionResult OnGetGet(DataSourceLoadOptions loadOptions)
         {
             return new JsonResult(DataSourceLoader.Load(SampleData.Orders, loadOptions));
+        }
+
+        public IActionResult OnPostAdd(string values)
+        {
+            var newItem = new SampleOrder();
+            newItem.OrderID = (SampleData.Orders.Max(t => (int?)t.OrderID) ?? 0) + 1;
+
+            JsonConvert.PopulateObject(values, newItem);
+
+            SampleData.Orders.Add(newItem);
+
+            return new OkObjectResult(newItem);
+        }
+
+        public IActionResult OnPutEdit(int key, string values)
+        {
+            var item = SampleData.Orders.Single(t => t.OrderID == key);
+
+            JsonConvert.PopulateObject(values, item);
+
+            return new OkObjectResult(item);
+        }
+
+        public IActionResult OnDeleteDelete(int key)
+        {
+            SampleData.Orders.RemoveAll(t => t.OrderID == key);
+
+            return new OkResult();
         }
 
         public IActionResult OnGetExportCsv()
