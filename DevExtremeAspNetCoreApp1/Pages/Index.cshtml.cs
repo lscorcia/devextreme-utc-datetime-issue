921fdb70e399fe454933fd7647cf4877a4005ee5
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2025-10-06 10:49:00 +0200

    Use razor page actions instead of a separate controller

--- DevExtremeAspNetCoreApp1/Pages/Index.cshtml.cs
+++ DevExtremeAspNetCoreApp1/Pages/Index.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using DevExtremeAspNetCoreApp1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +12,11 @@ namespace DevExtremeAspNetCoreApp1.Pages {
     public class IndexModel : PageModel {
         public void OnGet() {
 
+        }
+
+        public IActionResult OnGetGet(DataSourceLoadOptions loadOptions)
+        {
+            return new JsonResult(DataSourceLoader.Load(SampleData.Orders, loadOptions));
         }
     }
 }
